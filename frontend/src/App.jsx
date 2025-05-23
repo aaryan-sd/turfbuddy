9f01ba62e6ab04626ad0e0dfdb60a5611ef3dd6f
PiyushPowar88 <piyushpowar88@gmail.com>
2025-05-23 22:50:38 +0530

    Frontend setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,9 @@
+export default function App() {
+  return (
+    <div className="h-screen bg-gradient-to-r from-indigo-500 to-purple-600 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-white">
+        Tailwind CSS is working! 🎉
+      </h1>
+    </div>
+  );
+}
